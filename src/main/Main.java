554df5cfd8ede1d8979210679560e0a4d26ae32f
Isaac Lluís <isaaclluis@gmail.com>
2020-04-15 20:12:01 +0200

    First test

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Main extends BasicGame {
@@ -14,14 +15,17 @@ public class Main extends BasicGame {
 
     @Override
     public void init(GameContainer container) throws SlickException {
+    	
     }
 
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException {
+    	g.drawImage(new Image("assets/alive.png"), 25, 25);
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
+    	
     }
     
     public static void main(String[] args) throws SlickException {
